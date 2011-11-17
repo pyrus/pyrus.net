5bbe76cbec23131f18c6ea97e3bbe7a2b7317782
Brett Bieber <brett.bieber@gmail.com>
2011-11-17 09:45:50 -0600

    Basic website framework

---
+++ www/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pyrus</title>
+        <link rel="stylesheet" type="text/css" href="css/styles.css" />
+    </head>
+    <body>
+        <header>
+            <h1>Pyrus</h1>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+            </ul>
+        </nav>
+        <section>
+            
+        </section>
+        <footer>
+            &copy; <?php echo date('Y'); ?>
+        </footer>
+    </body>
+</html>
